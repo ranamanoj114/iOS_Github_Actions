3a7ef60d08b855080145423f06531d5259853760
ranamanoj114 <114919655+ranamanoj114@users.noreply.github.com>
2022-11-14 11:28:09 +0530

    Add files via upload

---
+++ Github_actionsTests/Github_actionsTests.swift
@@ -0,0 +1,24 @@
+//
+//  Github_actionsTests.swift
+//  Github_actionsTests
+//
+//  Created by Manoj Rana on 05/10/22.
+//
+
+import XCTest
+@testable import Github_actions
+
+class Github_actionsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+      XCTAssertTrue(1 == 1)
+    }
+}
